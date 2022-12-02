f5d40fa4b1b148f36a1cfa8a9094c152018169a3
Douglas Williams <douglaswilliams298@gmail.com>
2022-12-01 22:08:33 -0300

    Feat: Se aplicado esse commit adicionei os tipos, enum um console.log para fazer um test

--- revisao-mod5/src/endpoints/putCreateCharacter.ts
+++ revisao-mod5/src/endpoints/putCreateCharacter.ts
@@ -4,7 +4,9 @@ import {characters} from '../data'
 export default function putCreateCharacter(req: Request, res: Response): void {
 
     const {name, gender, description} = req.body
-    
+
+    console.log(req.headers["content-type"]) // vai retornar no terminal o tipo de conteúdo que está sendo enviado. No caso, "application/json"
+
     characters.push({
         id: Date.now(),  // Date.now() = pega o tempo em milisegundos e pode ser usado para gerar id
         name,
