203bb6662c45f7730d2b7ff7069576d9deb97ed4
Douglas Williams <douglaswilliams298@gmail.com>
2022-12-02 22:43:27 -0300

    Feat: Se aplicado esse commit adicionei dados do banco de dados no arquivo conections

--- revisao-mod5/src/endpoints/putCreateCharacter.ts
+++ revisao-mod5/src/endpoints/putCreateCharacter.ts
@@ -1,26 +1,26 @@
 import { Response, Request } from 'express'
-import { characters } from '../data'
+// import { characters } from '../data'
 
 export default function putCreateCharacter(req: Request, res: Response): void {
     let errorCode: number = 400;
-    try {
-        const { name, gender, description } = req.body
-        console.log(req.headers["content-type"]) // vai retornar no terminal o tipo de conteúdo que está sendo enviado. No caso, "application/json"
+    // try {
+    //     const { name, gender, description } = req.body
+    //     console.log(req.headers["content-type"]) // vai retornar no terminal o tipo de conteúdo que está sendo enviado. No caso, "application/json"
 
-        characters.push({
-            id: Date.now(),  // Date.now() = pega o tempo em milisegundos e pode ser usado para gerar id
-            name,
-            gender,
-            description
-        })
-        if (!name || !gender) {
-            errorCode = 442;
-            throw new Error("Please check the fields")
-        }
-    } catch (error) {
-        res.status(errorCode).end()
-    }
+    //     characters.push({
+    //         id: Date.now(),  // Date.now() = pega o tempo em milisegundos e pode ser usado para gerar id
+    //         name,
+    //         gender,
+    //         description
+    //     })
+    //     if (!name || !gender) {
+    //         errorCode = 442;
+    //         throw new Error("Please check the fields")
+    //     }
+    // } catch (error) {
+    //     res.status(errorCode).end()
+    // }
 
-    res.status(201).end()
+    // res.status(201).end()
 
 }
