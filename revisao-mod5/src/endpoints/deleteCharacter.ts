a899b8145df8c6a1464feac7f769e2b952e373b5
Douglas Williams <douglaswilliams298@gmail.com>
2022-12-02 21:06:36 -0300

    Feat: Se aplicado esse commit adicionei o endpoint delete, e adicionei as tratativas de erros nos demais endpoints

---
+++ revisao-mod5/src/endpoints/deleteCharacter.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { characters } from "../data";
+
+export default function deleteCharacter (req: Request, res: Response) {
+    let errorCode: number = 400;
+    try {
+        const id: number = Number(req.params.id);
+        const index: number = characters.findIndex((character) => character.id === id);
+        if (index === -1) {
+            errorCode = 404;
+            throw new Error("Character not found!");
+        }
+        characters.splice(index, 1);
+        res.status(200).send("Character deleted successfully!");
+    } catch (error) {
+        res.status(errorCode).end();
+    }
+}
--- revisao-mod5/src/endpoints/putCreateCharacter.ts
+++ revisao-mod5/src/endpoints/putCreateCharacter.ts
@@ -1,20 +1,26 @@
-import {Response, Request} from 'express'
-import {characters} from '../data'
+import { Response, Request } from 'express'
+import { characters } from '../data'
 
 export default function putCreateCharacter(req: Request, res: Response): void {
+    let errorCode: number = 400;
+    try {
+        const { name, gender, description } = req.body
+        console.log(req.headers["content-type"]) // vai retornar no terminal o tipo de conteúdo que está sendo enviado. No caso, "application/json"
 
-    const {name, gender, description} = req.body
-
-    console.log(req.headers["content-type"]) // vai retornar no terminal o tipo de conteúdo que está sendo enviado. No caso, "application/json"
-
-    characters.push({
-        id: Date.now(),  // Date.now() = pega o tempo em milisegundos e pode ser usado para gerar id
-        name,
-        gender,
-        description
-    })
+        characters.push({
+            id: Date.now(),  // Date.now() = pega o tempo em milisegundos e pode ser usado para gerar id
+            name,
+            gender,
+            description
+        })
+        if (!name || !gender) {
+            errorCode = 442;
+            throw new Error("Please check the fields")
+        }
+    } catch (error) {
+        res.status(errorCode).end()
+    }
 
     res.status(201).end()
-
 
 }
